464a2cd1ddeb8d0132a9cc50f6875f225895098f
ramakusuma10 <ramakusuma098@gmail.com>
2024-06-15 20:42:29 +0700

    fix : implement themes  circle app

--- src/themes/theme.ts
+++ src/themes/theme.ts
@@ -1,7 +1,46 @@
 import { extendTheme } from '@chakra-ui/react'
+import { inputAnatomy } from '@chakra-ui/anatomy'
+import { createMultiStyleConfigHelpers } from '@chakra-ui/react'
+const { definePartsStyle, defineMultiStyleConfig } = createMultiStyleConfigHelpers(
+    inputAnatomy.keys
+    )
+    
+    const hollow = definePartsStyle({
+    field: {
+        border: '1px solid',
+        borderColor: 'circle.dark',
+        background: 'transparent',
+        borderRadius: 'lg',
+        _hover: {
+            background: 'none',
+            boxShadow: 'none',
+            borderColor: 'circle.green',
+        },
+        _active: {
+            background: 'none',
+            boxShadow: 'none',
+            borderColor: 'circle.green',
+        },
+        _focus: {
+            background: 'none',
+            boxShadow: 'none',
+            borderColor: 'circle.green',
+        },
+        _autofill: {
+            transition: 'background-color 0s 600000s, color 0s 600000s',
+        },
+        _placeholder: {
+            color: 'circle.dark',
+        },
+    },
+    })
 
-const circleTheme = extendTheme({
-    colors: {
+    const inputTheme = defineMultiStyleConfig({
+        variants: { hollow },
+    })
+    
+    const circleTheme = extendTheme({
+        colors: {
         circle: {
             backdrop: '#171717',
             backdropGreen: '#1c1c1c',
@@ -11,6 +50,7 @@ const circleTheme = extendTheme({
             red: '#D71913',
             green: '#0be94ede',
             darkgreen: '#339438',
+            error: '#cc0000',
         },
     },
     styles: {
@@ -23,6 +63,8 @@ const circleTheme = extendTheme({
             },
         },
     },
+    components: { Input: inputTheme },
 })
+
 
 export default circleTheme
