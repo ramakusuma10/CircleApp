d5354df4aaeffcc29128608b6bd3c127fcfb8bd0
ramakusuma10 <ramakusuma098@gmail.com>
2024-06-15 20:44:45 +0700

    fix : implement Debounce Search  circle app

--- src/pages/searchpage.tsx
+++ src/pages/searchpage.tsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom'
-import { Flex, Grid, GridItem, Text, Avatar, Input } from '@chakra-ui/react'
-import { BiLeftArrowAlt } from 'react-icons/bi'
-import { fontSizing } from '../styles/style'
+import { Flex, Grid, GridItem, Text} from '@chakra-ui/react'
+import { BiLeftArrowAlt, BiSearchAlt } from 'react-icons/bi'
 
 import MainBar from '../component/bar/Mainbar'
 import SideBar from '../component/bar/Sidebar'
@@ -9,70 +8,110 @@ import ProfileCard from '../component/cards/ProfileCard'
 import SuggestionCard from '../component/cards/SuggestionCard'
 import DeveloperCard from '../component/cards/DeveloperCard'
 import MenuHeading from '../component/navigations/Menuheading'
+import AccountCard from '../component/cards/AccountCard'
 
-import HollowButton from '../component/button/hollowButton'
-
-import { useEffect, useState } from 'react'
-import { useDebounce } from 'use-debounce'
-import { UserSearch } from '../features/search/types/search'
-import { api }  from '../features/libs/api'
+import { useState } from 'react'
+import { useDebouncedCallback } from 'use-debounce'
+import api from '../libs/api'
+import { UserType } from '../types/types'
+import IconInput from '../component/input/IconInput'
+import { useForm } from 'react-hook-form'
 
 function SearchPage() {
-    const [searchInput, setSearchInput] = useState<string>("");
-  const [debouncedSearchInput] = useDebounce(searchInput, 400);
-  const [searchData, setSearchData] = useState<UserSearch[]>([]);
+    const { register, watch} = useForm()
+    const [searchInput, setSearchInput] = useState<string>('')
+    const [searchData, setSearchData] = useState<UserType[]>([])
 
-  async function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setSearchInput(e.target.value);
-  }
-
-  async function getData() {
-    const response = await api.get(`/users?search=${debouncedSearchInput}`);
-    setSearchData(response.data);
-  }
-
-  useEffect(() => {
-    getData();
-  }, [debouncedSearchInput]);
+    const debounced = useDebouncedCallback(async (searchInput) => {
+            try {
+                const searchData = await api.GetUsers(searchInput)
+                setSearchData(searchData)
+            } catch (error) {
+                throw new Error()
+            }
+    }, 500)
+        
+    watch((data) => {
+        setSearchData([])
+        setSearchInput(data.search)
+    
+        debounced(data.search)
+    })
+    
+    if(!searchInput){
+        return (
+            <Grid templateColumns={'repeat(19, 1fr)'}>
+                <GridItem colSpan={12}>
+                    <MainBar>
+                        <Link to={'/'}>
+                            <MenuHeading icon={<BiLeftArrowAlt />} text={'Search'} gummy />
+                        </Link>
+                        <IconInput
+                            type={'text'}
+                            placeholder={'search'}
+                            icon={<BiSearchAlt />}
+                            name={'search'}
+                            register={register}
+                        />
+                    </MainBar>
+                </GridItem>
+                <GridItem colSpan={7}>
+                    <SideBar>
+                        <ProfileCard />
+                        <SuggestionCard />
+                        <DeveloperCard />
+                    </SideBar>
+                </GridItem>
+            </Grid>
+        )
+    }
 
     return (
         <Grid templateColumns={'repeat(19, 1fr)'}>
             <GridItem colSpan={12}>
                 <MainBar>
                     <Link to={'/'}>
-                        <MenuHeading icon={<BiLeftArrowAlt />} text={'Search'} />
+                        <MenuHeading icon={<BiLeftArrowAlt />} text={'Search'} gummy />
                     </Link>
-                    <Input onChange={handleChange}/>
-                    {searchData.map((user) =>(
-                    <Flex direction={'column'} gap={'15px'} px={'15px'} mt={'15px'}>
-                        <Flex gap={'15px'} alignItems={'center'}>
-                            <Avatar src={user.avatar} />
-                            <Flex direction={'column'} justifyContent={'center'} gap={0} mr={'auto'}>
-                                <Text fontSize={fontSizing.small} fontWeight={'700'}>
-                                    {user.fullname}
+                    <IconInput
+                        type={'text'}
+                        placeholder={'search'}
+                        icon={<BiSearchAlt />}
+                        name={'search'}
+                        register={register}
+                    />
+                    <Flex direction={'column'} gap={'1rem'} px={'1rem'} mt={'1rem'}>
+                        {searchData.length ? (
+                            searchData.map((Data) => (
+                                <AccountCard
+                                    key={Data.id}
+                                    id={Data.id}
+                                    fullname={Data.fullname}
+                                    username={Data.username}
+                                    avatar={Data.avatar}
+                                    bio={Data.bio}
+                                    followed={Data.isFollowed}
+                                />
+                                ))
+                            ):(
+                                <Text>
+                                    Tidak Ada Data
                                 </Text>
-                                <Text fontSize={fontSizing.smaller} color={'circle.dark'}>
-                                    {user.username}
-                                </Text>
-                                <Text fontSize={fontSizing.smaller}>
-                                    {user.bio}
-                                </Text>
-                            </Flex>
-                            <HollowButton text={'Follow'} />
-                        </Flex>
+                            )}
+                            
                     </Flex>
-                    ))}
                 </MainBar>
             </GridItem>
             <GridItem colSpan={7}>
                 <SideBar>
-                    <ProfileCard top />
+                    <ProfileCard />
                     <SuggestionCard />
                     <DeveloperCard />
                 </SideBar>
             </GridItem>
         </Grid>
     )
+
 }
 
 export default SearchPage
