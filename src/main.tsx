809d3f59a6d26c117db6b21021acf7ce66eca8c5
ramakusuma10 <ramakusuma098@gmail.com>
2024-06-15 20:47:09 +0700

    fix : implement main circle app

--- src/main.tsx
+++ src/main.tsx
@@ -7,15 +7,20 @@ import { Provider } from 'react-redux'
 import { ChakraProvider } from '@chakra-ui/react'
 import circleTheme from './themes/theme.ts'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const QClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-       <Provider store={store}>
-            <ChakraProvider theme={circleTheme}>
-                <BrowserRouter>
-                    <App />
-                </BrowserRouter>
-            </ChakraProvider>
-        </Provider>
+        <QueryClientProvider client={QClient}>
+            <Provider store={store}>
+                <ChakraProvider theme={circleTheme}>
+                    <BrowserRouter>
+                        <App />
+                    </BrowserRouter>
+                </ChakraProvider>
+            </Provider>
+        </QueryClientProvider>
     </React.StrictMode>
 )
