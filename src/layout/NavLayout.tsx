e7e5d41abb578f1665f6dad09d58724f26d75789
ramakusuma10 <ramakusuma098@gmail.com>
2024-06-18 19:54:18 +0700

    fix :  implement layout circle app

--- src/layout/NavLayout.tsx
+++ src/layout/NavLayout.tsx
@@ -1,29 +1,20 @@
-import { Box, Grid, GridItem, useDisclosure } from '@chakra-ui/react'
+import {Grid, GridItem} from '@chakra-ui/react'
 import { Outlet } from 'react-router-dom'
 
-import BrandModal from '../component/assets/BrandModal'
-import NewThread from '../component/threads/Newthreads'
 import SideBar from '../component/bar/Sidebar'
 import Menu from '../component/navigations/Menu'
 
 function NavLayout() {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-
     return (
         <Grid templateColumns={'repeat(24, 1fr)'}>
             <GridItem colSpan={5}>
                 <SideBar>
-                    <Menu onOpen={onOpen} />
+                    <Menu/>
                 </SideBar>
             </GridItem>
             <GridItem colSpan={19}>
                 <Outlet />
             </GridItem>
-            <BrandModal isOpen={isOpen} onClose={onClose} size={'xl'}>
-                <Box pt={'8px'}>
-                    <NewThread placeholder={"What's on your mind?"} />
-                </Box>
-            </BrandModal>
         </Grid>
     )
 }
