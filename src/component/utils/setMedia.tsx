bbc9a79a38413be0fdf24ddcea88ad17b822ccb1
ramakusuma10 <ramakusuma098@gmail.com>
2024-06-28 19:42:15 +0700

    fix : implement utilies media

--- src/component/utils/setMedia.tsx
+++ src/component/utils/setMedia.tsx
@@ -1,31 +1,55 @@
-import { Grid, Image } from '@chakra-ui/react'
+import { Grid, Image, useDisclosure } from '@chakra-ui/react'
+import { ThreadType } from '../../types/types'
+import { useSearchParams } from 'react-router-dom'
+import ImageModal from '../assets/ImageModal'
+import GhostButton from '../button/ghostButton'
 
-function SetMedia() {
-    const dummies = []
-    const dummy = (
-        <Image
-            src={'https://cdn.idntimes.com/content-images/community/2020/06/tanjiro1-ed6519a91156c68fcf14ac252e4bba71_600x400.jpg'}
-            height={'100%'}
-            width={'100%'}
-            objectFit={'cover'}
-        />
-    )
-
-    for (let i = 0; i < 14; i++) {
-        dummies.push(dummy)
-    }
-
-    return (
-        <Grid
-            templateColumns={'repeat(3, 1fr)'}
-            templateRows={'repeat(1, 150px)'}
-            autoRows={'150px'}
-            gap={'8px'}
-            padding={'15px'}
-        >
-            {dummies.map((dummy) => dummy)}
-        </Grid>
-    )
+interface SetMediaProps {
+    threads: ThreadType[]
 }
 
+function SetMedia({ threads }: SetMediaProps) {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const [, setSearchParams] = useSearchParams()
+
+    function onImageClick(id: number): void {
+        setSearchParams({ threadId: String(id) })
+
+        onOpen()
+    }
+
+    if (threads.length) {
+        return (
+            <Grid
+                templateColumns={'repeat(3, 1fr)'}
+                templateRows={'repeat(1, 150px)'}
+                autoRows={'150px'}
+                gap={'8px'}
+                padding={'15px'}
+            >
+                {threads.map((thread) => {
+                    if (thread.image) {
+                        return (
+                            <GhostButton onClick={() => onImageClick(thread.id)} key={thread.id}>
+                                <Image
+                                    src={thread.image}
+                                    height={'100%'}
+                                    width={'100%'}
+                                    objectFit={'cover'}
+                                />
+                                {isOpen && (
+                                    <ImageModal
+                                        isOpen={isOpen}
+                                        onClose={onClose}
+                                        ThreadPhoto={thread.image}
+                                    />
+                                )}
+                            </GhostButton>
+                        )
+                    }
+                })}
+            </Grid>
+        )
+    }
+}
 export default SetMedia
