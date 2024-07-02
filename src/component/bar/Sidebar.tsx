562b382f249fdfdec29203fb4d28f8b6feefcd2a
ramakusuma10 <ramakusuma098@gmail.com>
2024-07-02 14:49:15 +0700

    fix : side bar

--- src/component/bar/Sidebar.tsx
+++ src/component/bar/Sidebar.tsx
@@ -6,7 +6,7 @@ interface SideBarProps {
 }
 
 function SideBar({ children }: SideBarProps): JSX.Element {
-    return <Box as="aside">{children}</Box>
+    return <Box as="aside" pos={'sticky'} top={0} py={'30px'}> {children}</Box>
 }
 
 export default SideBar
